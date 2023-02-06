92f768c11bb287eecea979d930ebaa5e259d1f5e
jsaravia <jsaravia@SARAVIALT>
2023-02-06 12:05:45 -0500

    Add project files.

---
+++ TestClass/TriangleTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TriangleSolver;
+namespace TestClass
+{
+    public class TriangleTest
+    {
+
+        [TestFixture]
+
+        public class TriangleTests
+        {
+            [Test]
+
+            public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+            {
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid.";
+
+                string actual = Triangle.AnalyzeTriangle(firstAngle, secondAngle, thirdAngle);
+
+            }
+
+
+            [Test]
+             public void ValidTriangle__OutputValidTriangle()
+            {
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid.";
+
+                string actual = Triangle.AnalyzeTriangle(firstAngle, secondAngle, thirdAngle);
+
+            }
+
+
+        }
+
+    }
+}
